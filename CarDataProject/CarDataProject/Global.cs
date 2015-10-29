1a759cff725908bbf6e64227aab6fe097f22b345
Morten <treeline@live.dk>
2015-10-29 09:49:07 +0100

    Det Lau ikke gad pushe

--- CarDataProject/CarDataProject/Global.cs
+++ CarDataProject/CarDataProject/Global.cs
@@ -9,6 +9,7 @@ namespace CarDataProject {
             public const string User = "casper";
             public const string Password = "1234";
         }
+
         public static class FileType {
             public const string txt = @".txt";
         }
