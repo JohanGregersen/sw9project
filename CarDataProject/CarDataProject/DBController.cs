fb7d74777f6bd653b5e54f5b4b8a702a18922589
Morten <treeline@live.dk>
2015-11-05 14:50:39 +0100

    Improved speeding statistics

--- CarDataProject/CarDataProject/DBController.cs
+++ CarDataProject/CarDataProject/DBController.cs
@@ -7,29 +7,29 @@ using System.Device.Location;
 
 namespace CarDataProject {
     public class DBController {
-        NpgsqlConnection connection;
-        DataSet dataSet = new DataSet();
-        DataTable dataTable = new DataTable();
+        NpgsqlConnection Connection;
+        DataSet DataSet = new DataSet();
+        DataTable DataTable = new DataTable();
 
         public DBController() {
             string connectionSettings = String.Format("Server={0};User Id={1};Password={2};Database={3};",
                 Global.Database.Host, Global.Database.User, Global.Database.Password, Global.Database.Name);
 
-            connection = new NpgsqlConnection(connectionSettings);
-            connection.Open();
+            Connection = new NpgsqlConnection(connectionSettings);
+            Connection.Open();
         }
 
         public void Close() {
-            connection.Close();
+            Connection.Close();
         }
 
         private DataRowCollection Query(string sql) {
-            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sql, connection);
-            dataSet.Reset();
-            dataAdapter.Fill(dataSet);
-            dataTable = dataSet.Tables[0];
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sql, Connection);
+            DataSet.Reset();
+            dataAdapter.Fill(DataSet);
+            DataTable = DataSet.Tables[0];
 
-            return dataTable.Rows;
+            return DataTable.Rows;
         }
 
         private int NonQuery(NpgsqlCommand command, string table) {
@@ -42,11 +42,12 @@ namespace CarDataProject {
         }
 
         #region Creators
+
         public int AddCar() {
             string sql = @"INSERT INTO carinformation 
                         DEFAULT VALUES ";
 
-            NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+            NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
 
             return NonQuery(command, "carinformation");
         }
@@ -55,7 +56,7 @@ namespace CarDataProject {
             string sql = @"INSERT INTO segmentinformation(segmentid, osmid, roadname, roadtype, oneway, speedbackward, speedforward, segmentline) 
                             VALUES (@segmentid, @osmid, @roadname, @roadtype, @oneway, @speedbackward, @speedforward, ST_GeomFromText(@lineString, 4326))";
 
-            NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+            NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
             command.Parameters.AddWithValue("@segmentid", segment.SegmentId);
             command.Parameters.AddWithValue("@osmid", segment.OSMId);
             command.Parameters.AddWithValue("@roadname", segment.RoadName);
@@ -79,7 +80,7 @@ namespace CarDataProject {
         public int AddQualityInformation(QualityInformation QI) {
             string sql = @"INSERT INTO qualityinformation(satellites, hdop) VALUES (@satellites, @hdop)";
 
-            NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+            NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
             command.Parameters.AddWithValue("@satellites", QI.Sat);
             command.Parameters.AddWithValue("@hdop", QI.Hdop);
 
@@ -90,7 +91,7 @@ namespace CarDataProject {
             string sql = @"INSERT INTO facttable(carid, segmentid, qualityid,dayofweek, idate, itime, point, mpoint, speed) 
                         VALUES (@carid, @segmentid, @qualityid, @dayofweek, @idate, @itime, ST_MakePoint(@point, 4326), ST_MakePoint(@mpoint, 4326), @speed)";
 
-            NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+            NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
             command.Parameters.AddWithValue("@carid", fact.CarId);
             command.Parameters.AddWithValue("@segmentid", fact.Segment.SegmentId);
             command.Parameters.AddWithValue("@qualityid", fact.Quality.EntryId);
@@ -126,7 +127,6 @@ namespace CarDataProject {
 
             */
         #endregion Creators
-
 
         #region Getters
 
@@ -274,7 +274,7 @@ namespace CarDataProject {
         }
 
         public List<Fact> GetSpeedInformationByCarIdAndTripId(Int16 carId, Int64 tripId) {
-            string sql = String.Format(@"SELECT entryid, itime, idate, speed, CASE 
+            string sql = String.Format(@"SELECT entryid, itime, idate, speed, distancetolag,  CASE 
                                             WHEN direction = TRUE THEN speedforward
                                             ELSE speedbackward
                                             END AS maxspeed
@@ -291,9 +291,11 @@ namespace CarDataProject {
                     int time = row.Field<int>("itime");
                     double speed = row.Field<double>("speed");
                     Int16 maxSpeed = row.Field<Int16>("maxspeed");
+                    double distanceToLag = row.Field<double>("distancetolag");
 
                     speedInformation.Add(new Fact(entryId, SegmentInformation.CreateWithMaxSpeed(maxSpeed),
                                        new TemporalInformation(DateTimeHelper.ConvertToDateTime(date, time)),
+                                       new SpatialInformation(distanceToLag),
                                        new MeasureInformation(speed)));
                 }
 
@@ -389,7 +391,7 @@ namespace CarDataProject {
 
         public int UpdateWithNewId(int newId, int currentId) {
             string sql = String.Format("UPDATE cardata SET newtripid = '{0}' WHERE id = '{1}'", newId, currentId);
-            NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+            NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
             return NonQuery(command, "cardata");
         }
 
@@ -402,7 +404,7 @@ namespace CarDataProject {
                 sb.Append(String.Format("OR id = '{0}'", currentIds[i].Item1));
             }
 
-            NpgsqlCommand command = new NpgsqlCommand(sb.ToString(), connection);
+            NpgsqlCommand command = new NpgsqlCommand(sb.ToString(), Connection);
             return NonQuery(command, "cardata");
         }
 
@@ -421,7 +423,7 @@ namespace CarDataProject {
 
             foreach (int entryId in entryIds) {
                 string sql2 = String.Format("UPDATE cardata SET point = ST_Transform(ST_SetSrid(ST_MakePoint(xcoord, ycoord), 32632), 4326), mpoint = ST_Transform(ST_SetSrid(ST_MakePoint(mpx, mpy), 32632), 4326) WHERE id = '{0}'", entryId);
-                NpgsqlCommand command = new NpgsqlCommand(sql2, connection);
+                NpgsqlCommand command = new NpgsqlCommand(sql2, Connection);
                 NonQuery(command, "cardata");
             }
         }
@@ -430,7 +432,7 @@ namespace CarDataProject {
             Int64 tripsTaken = CarStatistics.TripCount(carId);
             for (int i = 1; i < tripsTaken; i++) {
                 string sql = String.Format("UPDATE cardata SET mline = myline FROM(SELECT newtripid, id, ST_MakeLine(mpoint, next_mpoint) AS myline FROM(SELECT newtripid, id, mpoint, lead(mpoint) OVER w as next_mpoint, lead(newtripid) OVER w as next_newtripid FROM cardata WINDOW w AS(ORDER BY id)) AS res WHERE newtripid = '{0}' AND next_newtripid = newtripid) AS calclines WHERE cardata.id = calclines.id", i);
-                NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+                NpgsqlCommand command = new NpgsqlCommand(sql, Connection);
                 NonQuery(command, "cardata");
             }
         }
--- CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
+++ CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CarDataProject {
     public static class SpeedingStatistics {
-        public static TimeSpan Time(Int16 carId, Int64 tripId, Int16 minimumSpeedingAmount, TimeSpan ignorableSpeedingTime) {
+        public static TimeSpan Time(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan timeSped = new TimeSpan();
 
             DBController dbc = new DBController();
@@ -12,32 +12,88 @@ namespace CarDataProject {
 
             bool speeding = false;
             DateTime speedingStartPoint = new DateTime();
+            double speedingDistance = 0;
 
             //For the entire trip
             foreach (Fact fact in speedData) {
                 //If entry indicates speeding
-                if (fact.Measure.Speed >= fact.Segment.MaxSpeed + minimumSpeedingAmount) {
+                if (fact.Measure.Speed >= fact.Segment.MaxSpeed + ignorableSpeed) {
+                    speedingDistance += fact.Spatial.DistanceToLag;
 
                     //If not previously speeding, remember the time where driver began speeding
                     if (!speeding) {
-                        speeding = true;
                         speedingStartPoint = fact.Temporal.Timestamp;
+                        speeding = true;
                     }
 
                 //If driver is not speeding
                 } else {
                     //If previously speeding, add the speeding-time to the total
                     if (speeding) {
-                        speeding = false;
-
-                        if (fact.Temporal.Timestamp - speedingStartPoint >= ignorableSpeedingTime) {
+                        if (fact.Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
                             timeSped += fact.Temporal.Timestamp - speedingStartPoint;
                         }
+                        speedingDistance = 0;
+                        speeding = false;
                     }
                 }
             }
 
+            //Handle case where last entry in trip is still speeding
+            if (speeding) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                    timeSped += speedData[speedData.Count - 1].Temporal.Timestamp - speedingStartPoint;
+                }
+            }
+
             return timeSped;
+        }
+
+        public static double Distance(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double distanceSped = 0;
+
+            DBController dbc = new DBController();
+            List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
+            dbc.Close();
+
+            bool speeding = false;
+            DateTime speedingStartPoint = new DateTime();
+            double speedingDistance = 0;
+
+            //For the entire trip
+            foreach (Fact fact in speedData) {
+                //If entry indicates speeding
+                if (fact.Measure.Speed >= fact.Segment.MaxSpeed + ignorableSpeed) {
+                    speedingDistance += fact.Spatial.DistanceToLag;
+
+                    //If not previously speeding, remember the time where driver began speeding
+                    if (!speeding) {
+                        speedingStartPoint = fact.Temporal.Timestamp;
+                        speeding = true;
+                    }
+
+                    //If driver is not speeding
+                } else {
+                    //If previously speeding, add the speeding-time to the total
+                    if (speeding) {
+                        if (fact.Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                            distanceSped += speedingDistance;
+                        }
+
+                        speedingDistance = 0;
+                        speeding = false;
+                    }
+                }
+            }
+
+            //Handle case where last entry in trip is still speeding
+            if (speeding) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                    distanceSped += speedingDistance;
+                }
+            }
+
+                return distanceSped;
         }
     }
 }
