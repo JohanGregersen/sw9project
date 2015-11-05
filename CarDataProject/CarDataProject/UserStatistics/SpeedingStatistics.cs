da33c7026f412dff33b25c78e4cf0eebe3036b44
Morten <treeline@live.dk>
2015-11-05 15:31:24 +0100

    så er der dælme speedingstatistics

--- CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
+++ CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CarDataProject {
     public static class SpeedingStatistics {
-        public static TimeSpan Time(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static TimeSpan TimeAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan timeSped = new TimeSpan();
 
             DBController dbc = new DBController();
@@ -11,7 +11,7 @@ namespace CarDataProject {
             dbc.Close();
 
             bool speeding = false;
-            DateTime speedingStartPoint = new DateTime();
+            DateTime speedingStartPoint = speedData[0].Temporal.Timestamp;
             double speedingDistance = 0;
 
             //For the entire trip
@@ -49,7 +49,53 @@ namespace CarDataProject {
             return timeSped;
         }
 
-        public static double Distance(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static TimeSpan TimeBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            TimeSpan totalTimeBelow = new TimeSpan();
+
+            DBController dbc = new DBController();
+            List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
+            dbc.Close();
+
+            bool belowLimit = false;
+            DateTime belowLimitStartPoint = speedData[0].Temporal.Timestamp;
+            double distanceBelowLimit = 0;
+
+            //For the entire trip
+            foreach (Fact fact in speedData) {
+                //If entry indicates not speeding
+                if (fact.Measure.Speed <= fact.Segment.MaxSpeed + ignorableSpeed) {
+                    distanceBelowLimit += fact.Spatial.DistanceToLag;
+
+                    //If not previously below limit, remember the time where driver got below limit
+                    if (!belowLimit) {
+                        belowLimitStartPoint = fact.Temporal.Timestamp;
+                        belowLimit = true;
+                    }
+
+                    //If driver is speeding
+                } else {
+                    //If previously below limit, add the time below limit to the total
+                    if (belowLimit) {
+                        if (fact.Temporal.Timestamp - belowLimitStartPoint >= ignorableTime && distanceBelowLimit > ignorableDistance) {
+                            totalTimeBelow += fact.Temporal.Timestamp - belowLimitStartPoint;
+                        }
+                        distanceBelowLimit = 0;
+                        belowLimit = false;
+                    }
+                }
+            }
+
+            //Handle case where last entry in trip is below limit
+            if (belowLimit) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - belowLimitStartPoint >= ignorableTime && distanceBelowLimit > ignorableDistance) {
+                    totalTimeBelow += speedData[speedData.Count - 1].Temporal.Timestamp - belowLimitStartPoint;
+                }
+            }
+
+            return totalTimeBelow;
+        }
+
+        public static double DistanceAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             double distanceSped = 0;
 
             DBController dbc = new DBController();
@@ -57,7 +103,7 @@ namespace CarDataProject {
             dbc.Close();
 
             bool speeding = false;
-            DateTime speedingStartPoint = new DateTime();
+            DateTime speedingStartPoint = speedData[0].Temporal.Timestamp;
             double speedingDistance = 0;
 
             //For the entire trip
@@ -94,6 +140,80 @@ namespace CarDataProject {
             }
 
                 return distanceSped;
+        }
+
+        public static double DistanceBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double totalDistanceBelow = 0;
+
+            DBController dbc = new DBController();
+            List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
+            dbc.Close();
+
+            bool belowLimit = false;
+            DateTime belowLimitStartPoint = speedData[0].Temporal.Timestamp;
+            double distanceBelowLimit = 0;
+
+            //For the entire trip
+            foreach (Fact fact in speedData) {
+                //If entry indicates not speeding
+                if (fact.Measure.Speed <= fact.Segment.MaxSpeed + ignorableSpeed) {
+                    distanceBelowLimit += fact.Spatial.DistanceToLag;
+
+                    //If not previously below limit, remember the time where driver got below limit
+                    if (!belowLimit) {
+                        belowLimitStartPoint = fact.Temporal.Timestamp;
+                        belowLimit = true;
+                    }
+
+                    //If driver is speeding
+                } else {
+                    //If previously below limit, add the time below limit to the total
+                    if (belowLimit) {
+                        if (fact.Temporal.Timestamp - belowLimitStartPoint >= ignorableTime && distanceBelowLimit > ignorableDistance) {
+                            totalDistanceBelow += distanceBelowLimit;
+                        }
+                        distanceBelowLimit = 0;
+                        belowLimit = false;
+                    }
+                }
+            }
+
+            //Handle case where last entry in trip is below limit
+            if (belowLimit) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - belowLimitStartPoint >= ignorableTime && distanceBelowLimit > ignorableDistance) {
+                    totalDistanceBelow += distanceBelowLimit;
+                }
+            }
+
+            return totalDistanceBelow;
+        }
+
+        public static double PercentageTimeAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            TimeSpan timeAbove = TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+            TimeSpan timeBelow = TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+
+            return timeAbove.Ticks / (timeAbove.Ticks + timeBelow.Ticks) * 100;
+        }
+
+        public static double PercentageTimeBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            TimeSpan timeAbove = TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+            TimeSpan timeBelow = TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+
+            return timeBelow.Ticks / (timeAbove.Ticks + timeBelow.Ticks) * 100;
+        }
+
+        public static double PercentageDistanceAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double distanceAbove = DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+            double distanceBelow = DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+
+            return distanceAbove / (distanceAbove + distanceBelow) * 100;
+        }
+
+        public static double PercentageDistanceBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double distanceAbove = DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+            double distanceBelow = DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+
+            return distanceBelow / (distanceAbove + distanceBelow) * 100;
         }
     }
 }
