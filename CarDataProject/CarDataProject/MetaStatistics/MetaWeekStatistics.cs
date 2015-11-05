9ffb50e6608c969a9990d2f2fc8f7e254fb104d1
Morten <treeline@live.dk>
2015-11-05 13:18:15 +0100

    I pushed shit

--- CarDataProject/CarDataProject/MetaStatistics/MetaWeekStatistics.cs
+++ CarDataProject/CarDataProject/MetaStatistics/MetaWeekStatistics.cs
@@ -35,12 +35,12 @@ namespace CarDataProject {
         public static Dictionary<DayOfWeek, TimeSpan> AverageTripTimePerDay() {
             Dictionary<DayOfWeek, TimeSpan> timePerDay = new Dictionary<DayOfWeek, TimeSpan>();
             timePerDay.Add(DayOfWeek.Monday, new TimeSpan(0, 0, 0));
-            timePerDay.Add(DayOfWeek.Tuesday, new TimeSpan(0, 0, 0)));
-            timePerDay.Add(DayOfWeek.Wednesday, new TimeSpan(0, 0, 0)));
-            timePerDay.Add(DayOfWeek.Thursday, new TimeSpan(0, 0, 0)));
-            timePerDay.Add(DayOfWeek.Friday, new TimeSpan(0, 0, 0)));
-            timePerDay.Add(DayOfWeek.Saturday, new TimeSpan(0, 0, 0)));
-            timePerDay.Add(DayOfWeek.Sunday, new TimeSpan(0, 0, 0)));
+            timePerDay.Add(DayOfWeek.Tuesday, new TimeSpan(0, 0, 0));
+            timePerDay.Add(DayOfWeek.Wednesday, new TimeSpan(0, 0, 0));
+            timePerDay.Add(DayOfWeek.Thursday, new TimeSpan(0, 0, 0));
+            timePerDay.Add(DayOfWeek.Friday, new TimeSpan(0, 0, 0));
+            timePerDay.Add(DayOfWeek.Saturday, new TimeSpan(0, 0, 0));
+            timePerDay.Add(DayOfWeek.Sunday, new TimeSpan(0, 0, 0));
 
             DBController dbc = new DBController();
             List<Int16> carIds = dbc.GetCarIds();
