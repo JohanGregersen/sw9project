26b05bd052de7992cdd5b9e972a2e4b2d0439951
Morten <treeline@live.dk>
2015-11-06 12:29:40 +0100

    Untested statistics

--- CarDataProject/CarDataProject/MetaStatistics/MetaSpeedingStatistics.cs
+++ CarDataProject/CarDataProject/MetaStatistics/MetaSpeedingStatistics.cs
@@ -1,10 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarDataProject {
     class MetaSpeedingStatistics {
+        public static double AveragePercentageTimeAbove(double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            TimeSpan totalTimeAbove = new TimeSpan(0, 0, 0);
+            TimeSpan totalTimeBelow = new TimeSpan(0, 0, 0);
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    totalTimeAbove += SpeedingStatistics.TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                    totalTimeBelow += SpeedingStatistics.TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                }
+            }
+
+            dbc.Close();
+            return totalTimeAbove.Ticks / (totalTimeAbove.Ticks + totalTimeBelow.Ticks) * 100;
+        }
+
+        public static double AveragePercentageTimeBelow(double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            TimeSpan totalTimeAbove = new TimeSpan(0, 0, 0);
+            TimeSpan totalTimeBelow = new TimeSpan(0, 0, 0);
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    totalTimeAbove += SpeedingStatistics.TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                    totalTimeBelow += SpeedingStatistics.TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                }
+            }
+
+            dbc.Close();
+            return totalTimeBelow.Ticks / (totalTimeAbove.Ticks + totalTimeBelow.Ticks) * 100;
+        }
+
+        public static double AveragePercentageDistanceAbove(double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double totalDistanceAbove = 0;
+            double totalDistanceBelow = 0;
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    totalDistanceAbove += SpeedingStatistics.DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                    totalDistanceBelow += SpeedingStatistics.DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                }
+            }
+
+            dbc.Close();
+            return totalDistanceAbove / (totalDistanceAbove + totalDistanceBelow) * 100;
+        }
+
+        public static double AveragePercentageDistanceBelow(double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            double totalDistanceAbove = 0;
+            double totalDistanceBelow = 0;
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    totalDistanceAbove += SpeedingStatistics.DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                    totalDistanceBelow += SpeedingStatistics.DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
+                }
+            }
+
+            dbc.Close();
+            return totalDistanceBelow / (totalDistanceAbove + totalDistanceBelow) * 100;
+        }
+
+        public static double AveragePercentageTimeAboveInThreshold(double lowerPercentage, double upperPercentage, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            List<double> percentages = new List<double>();
+
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    percentages.Add(SpeedingStatistics.PercentageTimeAboveInThreshold(carId, tripId, lowerPercentage, upperPercentage, ignorableSpeed, ignorableTime, ignorableDistance));
+                }
+            }
+
+            double totalPercentage = 0;
+            foreach (double percentage in percentages) {
+                totalPercentage += percentage;
+            }
+
+            dbc.Close();
+            return totalPercentage / percentages.Count;
+        }
+
+        public static double AveragePercentageDistanceAboveInThreshold(double lowerPercentage, double upperPercentage, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            List<double> percentages = new List<double>();
+
+            DBController dbc = new DBController();
+            List<Int16> carIds = dbc.GetCarIds();
+
+            foreach (Int16 carId in carIds) {
+                List<Int64> tripIds = dbc.GetTripIdsByCarId(carId);
+                foreach (Int64 tripId in tripIds) {
+                    percentages.Add(SpeedingStatistics.PercentageDistanceAboveInThreshold(carId, tripId, lowerPercentage, upperPercentage, ignorableSpeed, ignorableTime, ignorableDistance));
+                }
+            }
+
+            double totalPercentage = 0;
+            foreach (double percentage in percentages) {
+                totalPercentage += percentage;
+            }
+
+            dbc.Close();
+            return totalPercentage / percentages.Count;
+        }
     }
 }
--- CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
+++ CarDataProject/CarDataProject/UserStatistics/SpeedingStatistics.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 
 namespace CarDataProject {
     public static class SpeedingStatistics {
-        public static TimeSpan TimeAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static TimeSpan TimeAbove(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan timeSped = new TimeSpan();
 
             DBController dbc = new DBController();
             List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
             dbc.Close();
 
-            bool speeding = false;
             DateTime speedingStartPoint = speedData[0].Temporal.Timestamp;
             double speedingDistance = 0;
+            bool speeding = false;
 
             //For the entire trip
             foreach (Fact fact in speedData) {
                 //If entry indicates speeding
-                if (fact.Measure.Speed >= fact.Segment.MaxSpeed + ignorableSpeed) {
+                if (fact.Measure.Speed > fact.Segment.MaxSpeed + ignorableSpeed) {
                     speedingDistance += fact.Spatial.DistanceToLag;
 
                     //If not previously speeding, remember the time where driver began speeding
@@ -49,7 +49,7 @@ namespace CarDataProject {
             return timeSped;
         }
 
-        public static TimeSpan TimeBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static TimeSpan TimeBelow(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan totalTimeBelow = new TimeSpan();
 
             DBController dbc = new DBController();
@@ -95,7 +95,7 @@ namespace CarDataProject {
             return totalTimeBelow;
         }
 
-        public static double DistanceAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double DistanceAbove(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             double distanceSped = 0;
 
             DBController dbc = new DBController();
@@ -109,7 +109,7 @@ namespace CarDataProject {
             //For the entire trip
             foreach (Fact fact in speedData) {
                 //If entry indicates speeding
-                if (fact.Measure.Speed >= fact.Segment.MaxSpeed + ignorableSpeed) {
+                if (fact.Measure.Speed > fact.Segment.MaxSpeed + ignorableSpeed) {
                     speedingDistance += fact.Spatial.DistanceToLag;
 
                     //If not previously speeding, remember the time where driver began speeding
@@ -142,7 +142,7 @@ namespace CarDataProject {
                 return distanceSped;
         }
 
-        public static double DistanceBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double DistanceBelow(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             double totalDistanceBelow = 0;
 
             DBController dbc = new DBController();
@@ -188,32 +188,156 @@ namespace CarDataProject {
             return totalDistanceBelow;
         }
 
-        public static double PercentageTimeAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double PercentageTimeAbove(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan timeAbove = TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
             TimeSpan timeBelow = TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
 
             return timeAbove.Ticks / (timeAbove.Ticks + timeBelow.Ticks) * 100;
         }
 
-        public static double PercentageTimeBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double PercentageTimeBelow(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             TimeSpan timeAbove = TimeAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
             TimeSpan timeBelow = TimeBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
 
             return timeBelow.Ticks / (timeAbove.Ticks + timeBelow.Ticks) * 100;
         }
 
-        public static double PercentageDistanceAbove(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double PercentageDistanceAbove(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             double distanceAbove = DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
             double distanceBelow = DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
 
             return distanceAbove / (distanceAbove + distanceBelow) * 100;
         }
 
-        public static double PercentageDistanceBelow(Int16 carId, Int64 tripId, Int16 ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+        public static double PercentageDistanceBelow(Int16 carId, Int64 tripId, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
             double distanceAbove = DistanceAbove(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
             double distanceBelow = DistanceBelow(carId, tripId, ignorableSpeed, ignorableTime, ignorableDistance);
 
             return distanceBelow / (distanceAbove + distanceBelow) * 100;
+        }
+
+        public static double PercentageTimeAboveInThreshold(Int16 carId, Int64 tripId, double lowerPercentage, double upperPercentage, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            lowerPercentage += 100;
+            upperPercentage += 100;
+            TimeSpan totalTimeInThreshold = new TimeSpan(0, 0, 0);
+            TimeSpan totalTimeOutsideThreshold = new TimeSpan(0, 0, 0);
+
+            DBController dbc = new DBController();
+            List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
+            dbc.Close();
+
+            DateTime speedingStartPoint = speedData[0].Temporal.Timestamp;
+            double speedingDistance = 0;
+            TimeSpan timeInThreshold = new TimeSpan(0, 0, 0);
+            TimeSpan timeOutsideThreshold = new TimeSpan(0, 0, 0);
+            bool speeding = false;
+
+            //For the entire trip
+            for (int i = 1; i < speedData.Count; i++) {
+                //If entry indicates speeding
+                double percentageOfMaxSpeed = ((speedData[i].Measure.Speed - ignorableSpeed) / speedData[i].Segment.MaxSpeed) * 100;
+
+                if (percentageOfMaxSpeed > 100) {
+                    speedingDistance += speedData[i].Spatial.DistanceToLag;
+
+                    if (percentageOfMaxSpeed >= lowerPercentage && percentageOfMaxSpeed <= upperPercentage) {
+                        timeInThreshold += speedData[i].Temporal.TimeToLag;
+                    } else {
+                        timeOutsideThreshold += speedData[i].Temporal.TimeToLag;
+                    }
+                    //If not previously speeding, remember the time where driver began speeding
+                    if (!speeding) {
+                        speedingStartPoint = speedData[i].Temporal.Timestamp;
+                        speeding = true;
+                    }
+
+                    //If driver is not speeding
+                } else {
+                    //If previously speeding, add the speeding-time to the total
+                    if (speeding) {
+                        if (speedData[i].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                            totalTimeInThreshold += timeInThreshold;
+                            totalTimeOutsideThreshold += timeOutsideThreshold;
+                        }
+                        timeInThreshold = new TimeSpan(0, 0, 0);
+                        timeOutsideThreshold = new TimeSpan(0, 0, 0);
+                        speedingDistance = 0;
+                        speeding = false;
+                    }
+                }
+            }
+
+            //Handle case where last entry in trip is still speeding
+            if (speeding) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                    totalTimeInThreshold += timeInThreshold;
+                    totalTimeOutsideThreshold += timeOutsideThreshold;
+                }
+            }
+
+            return totalTimeInThreshold.Ticks / (totalTimeInThreshold.Ticks + totalTimeOutsideThreshold.Ticks) * 100;
+        }
+
+        public static double PercentageDistanceAboveInThreshold(Int16 carId, Int64 tripId, double lowerPercentage, double upperPercentage, double ignorableSpeed, TimeSpan ignorableTime, double ignorableDistance) {
+            lowerPercentage += 100;
+            upperPercentage += 100;
+            double totalDistanceInThreshold = 0;
+            double totalDistanceOutsideThreshold = 0;
+
+            DBController dbc = new DBController();
+            List<Fact> speedData = dbc.GetSpeedInformationByCarIdAndTripId(carId, tripId);
+            dbc.Close();
+
+            DateTime speedingStartPoint = speedData[0].Temporal.Timestamp;
+            double speedingDistance = 0;
+            double distanceInThreshold = 0;
+            double distanceOutsideThreshold = 0;
+            bool speeding = false;
+
+            //For the entire trip
+            for (int i = 1; i < speedData.Count; i++) {
+                //If entry indicates speeding
+                double percentageOfMaxSpeed = ((speedData[i].Measure.Speed - ignorableSpeed) / speedData[i].Segment.MaxSpeed) * 100;
+
+                if (percentageOfMaxSpeed > 100) {
+                    speedingDistance += speedData[i].Spatial.DistanceToLag;
+
+                    if (percentageOfMaxSpeed >= lowerPercentage && percentageOfMaxSpeed <= upperPercentage) {
+                        distanceInThreshold += speedData[i].Spatial.DistanceToLag;
+                    } else {
+                        distanceOutsideThreshold += speedData[i].Spatial.DistanceToLag;
+                    }
+                    //If not previously speeding, remember the time where driver began speeding
+                    if (!speeding) {
+                        speedingStartPoint = speedData[i].Temporal.Timestamp;
+                        speeding = true;
+                    }
+
+                    //If driver is not speeding
+                } else {
+                    //If previously speeding, add the speeding-distance to the total
+                    if (speeding) {
+                        if (speedData[i].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                            totalDistanceInThreshold += distanceInThreshold;
+                            totalDistanceOutsideThreshold += distanceOutsideThreshold;
+                        }
+                        distanceInThreshold = 0;
+                        distanceOutsideThreshold = 0;
+                        speedingDistance = 0;
+                        speeding = false;
+                    }
+                }
+            }
+
+            //Handle case where last entry in trip is still speeding
+            if (speeding) {
+                if (speedData[speedData.Count - 1].Temporal.Timestamp - speedingStartPoint >= ignorableTime && speedingDistance > ignorableDistance) {
+                    totalDistanceInThreshold += distanceInThreshold;
+                    totalDistanceOutsideThreshold += distanceOutsideThreshold;
+                }
+            }
+
+            return totalDistanceInThreshold / (totalDistanceInThreshold + totalDistanceOutsideThreshold) * 100;
         }
     }
 }
