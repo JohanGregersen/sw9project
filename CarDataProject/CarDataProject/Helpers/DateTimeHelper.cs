7587e3d9794f508bb066a30a20937cf5d7f4099e
Casper Laustsen <casper.laustsen@gmail.com>
2015-11-20 05:00:00 +0100

    Updated DateTimeHelper to also handle smartkeys with 8-digits

--- CarDataProject/CarDataProject/Helpers/DateTimeHelper.cs
+++ CarDataProject/CarDataProject/Helpers/DateTimeHelper.cs
@@ -4,7 +4,33 @@ namespace CarDataProject {
     public static class DateTimeHelper {
         public static DateTime ConvertToDateTime(int date, int time) {
             string dateString = date.ToString("D6");
+            Int16 year = 0;
+            Int16 month = 0;
+            Int16 day = 0;
+            if (dateString.Length == 8) {
+                year = Int16.Parse(dateString.Substring(0, 4));
+                month = Int16.Parse(dateString.Substring(4, 2));
+                day = Int16.Parse(dateString.Substring(6, 2));
+
+            } else {
+                year = Int16.Parse(dateString.Substring(4, 2));
+                month = Int16.Parse(dateString.Substring(2, 2));
+                day = Int16.Parse(dateString.Substring(0, 2));
+
+                //Year is represented in two digits only
+                year += 2000;
+            }
+
             string timeString = time.ToString("D6");
+            Int16 hour = Int16.Parse(timeString.Substring(0, 2));
+            Int16 minute = Int16.Parse(timeString.Substring(2, 2));
+            Int16 second = Int16.Parse(timeString.Substring(4, 2));
+
+            return new DateTime(year, month, day, hour, minute, second);
+        }
+
+        public static DateTime ConvertToDateTime(int date) {
+            string dateString = date.ToString("D6");
 
             Int16 year = Int16.Parse(dateString.Substring(4, 2));
             Int16 month = Int16.Parse(dateString.Substring(2, 2));
@@ -13,11 +39,7 @@ namespace CarDataProject {
             //Year is represented in two digits only
             year += 2000;
 
-            Int16 hour = Int16.Parse(timeString.Substring(0, 2));
-            Int16 minute = Int16.Parse(timeString.Substring(2, 2));
-            Int16 second = Int16.Parse(timeString.Substring(4, 2));
-
-            return new DateTime(year, month, day, hour, minute, second);
+            return new DateTime(year, month, day);
         }
 
         public static Int64 ToUnixTime(DateTime date) {
