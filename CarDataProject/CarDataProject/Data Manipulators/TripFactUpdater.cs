db4942725eb60ccd4263cfa86e996507f884b9f8
Casper Laustsen <casper.laustsen@gmail.com>
2015-11-26 14:41:53 +0100

    deleted scoreinformation. No longer in the database-schema Started working on the trip-class for tripfact Created the attached tripfactupdater

---
+++ CarDataProject/CarDataProject/Data Manipulators/TripFactUpdater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDataProject {
+    public class TripFactUpdater {
+        public static void Update(Int16 CarId) {
+
+            
+        }
+
+        private static List<Fact> UpdatedFacts(List<Fact> facts) {
+        }
+    }
+}
--- CarDataProject/CarDataProject/Objects/TripObjects/Trip.cs
+++ CarDataProject/CarDataProject/Objects/TripObjects/Trip.cs
@@ -3,14 +3,50 @@
 namespace CarDataProject {
     public class Trip {
         public Int64 TripId { get; }
+        public Int64 PreviousTripId { get; }
         public int CarId { get; }
-        public ScoreInformation Score { get; }
+        public TemporalInformation StartTemporal { get; }
+        public TemporalInformation EndTemporal { get; }
         public TimeSpan SecondsDriven { get; }
         public double MetersDriven { get; }
         public double Price { get; }
         public double BaselineScore { get; }
         public double OptimalityScore { get; }
+        public Int16 JerkCount { get; }
+        public Int16 BrakeCount { get; }
+        public Int16 AccelerationCount { get; }
+        public double MetersSped { get; }
+        public double TimeSped { get; }
+        public double SteadySpeedDistance { get; }
+        public double SteadySpeedTime { get; }
+        public TimeSpan SecondsToLag { get; }
+        public double DataQuality { get; }
 
+        /*
+
+              tripid bigserial NOT NULL,
+              previoustripid bigint,
+              carid integer,
+              startdate integer,
+              enddate integer,
+              starttime integer,
+              endtime integer,
+              secondsdriven integer,
+              metersdriven real,
+              price real,
+              optimalscore real,
+              tripscore real,
+              jerkcount smallint,
+              brakecount smallint,
+              accelerationcount smallint,
+              meterssped real,
+              timesped real,
+              steadyspeeddistance real,
+              steadyspeedtime real,
+              secondstolag integer,
+              dataquality real,
+
+        */
         public Trip (Int64 TripId, int CarId, TimeSpan SecondsDriven, double MetersDriven, double Price, double BaselineScore, double OptimalityScore) {
             this.TripId = TripId;
             this.CarId = CarId;
