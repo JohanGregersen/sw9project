5cf9e219433596086995f091bbbfbc7d7f7f6eed
Casper Laustsen <casper.laustsen@gmail.com>
2016-04-08 13:13:12 +0200

    er det den her?

---
+++ CarDataProject/CarDataProject/Map-Matching/Mapmatch.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace CarDataProject {
+    class Mapmatch {
+        private const string APPID = "496d0607";
+        private const string APPKEY = "c8a39964a19641b526ff3eef7d39d272";
+
+        public Mapmatch() {
+            DBController dbc = new DBController();
+            List<Fact> facts = dbc.GetFactsForMapByCarIdAndTripId(1, 44);
+            dbc.Close();
+            string postData = "";
+            
+
+            //CSV FORMAT
+            foreach (Fact f in facts) {
+                postData += f.EntryId + "," + f.Spatial.MPoint.Longitude.ToString().Replace(",",".") + "," + f.Spatial.MPoint.Latitude.ToString().Replace(",", ".") + ",\"" + f.Temporal.Timestamp.ToString("yyyy-MM-dd") + "T" + f.Temporal.Timestamp.TimeOfDay + "\"" + System.Environment.NewLine;
+            }
+            
+
+            //postData = ConvertToXML(facts);
+
+
+            // Create a request using a URL that can receive a post. 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://test.roadmatching.com/rest/mapmatch/?app_id=" + APPID + "&app_key=" + APPKEY + "&output.groupByWays=false&output.linkGeometries=false&output.osmProjection=false&output.linkMatchingError=false&output.waypoints=true&output.waypointsIds=true");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+
+            request.ContentType = "text/csv";
+            //request.ContentType = "application/gpx+xml";
+
+            request.Accept = "application/json";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine("Server response: " + responseFromServer);
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+        }
+
+
+
+        private static string ConvertToXML(List<Fact> facts) {
+
+            StringBuilder sb = new StringBuilder();
+
+
+            sb.Append("<? xml version = \" 1.0 \" encoding = \"UTF-8\" ?>");
+            sb.Append("< gpx xmlns = \"http://www.topografix.com/GPX/1/1 \" version = \"1.1\" creator = \"StudApp\" xmlns: xsi = \"http://www.w3.org/2001/XMLSchema-instance \" xsi: schemaLocation = \"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd \" >");
+            sb.Append("<trk>");
+            sb.Append("<name>StudApp</name>");
+            //< cmt >< ![CDATA[Warning: HDOP values aren't the HDOP as returned by the GPS device. They're approximated from the location accuracy in meters.]] ></ cmt >
+            sb.Append("<trkseg>");
+            foreach (Fact f in facts) {
+                sb.Append("<trkpt lat=\"");
+                sb.Append(f.Spatial.MPoint.Latitude.ToString().Replace(",", "."));
+                sb.Append("\" lon=\"");
+                sb.Append(f.Spatial.MPoint.Longitude.ToString().Replace(",", "."));
+                sb.Append("\">");
+
+                sb.Append("<time>");
+                sb.Append(f.Temporal.Timestamp.ToString("yyyy-MM-dd") + "T" + f.Temporal.Timestamp.TimeOfDay + "Z");
+                sb.Append("</time>");
+
+                sb.Append("<name>");
+                sb.Append(f.EntryId);
+                sb.Append("</name>");
+                sb.Append("</trkpt>");
+            }
+
+            sb.Append("</trkseg>");
+            sb.Append("</trk>");
+            sb.Append("</gpx>");
+
+            return sb.ToString();
+        }
+    }
+}
