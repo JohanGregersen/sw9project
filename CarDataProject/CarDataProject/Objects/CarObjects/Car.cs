4c61dfde57227460fc342b902a81cfb25fc98e69
Casper Laustsen <casper.laustsen@gmail.com>
2016-04-22 15:08:38 +0200

    Added functionality to add new cars, and get cars by imi.

--- CarDataProject/CarDataProject/Objects/CarObjects/Car.cs
+++ CarDataProject/CarDataProject/Objects/CarObjects/Car.cs
@@ -3,7 +3,7 @@ using System.Data;
 
 namespace CarDataProject {
     public class Car {
-        public int CarId { get; }
+        public Int16 CarId { get; }
         public string CarType { get; }
         public string Brand { get; }
         public string Model { get; }
@@ -14,7 +14,12 @@ namespace CarDataProject {
         public Int64 IMI { get; set; }
         public string Username { get; set; }
 
-        public Car(int CarId, string CarType, string Brand, string Model, double FuelConsumption, double EnergyConsumption, double Weight, Int16 Capacity, Int64 IMI, string Username) {
+        public Car(Int16 CarId, Int64 IMI) {
+            this.CarId = CarId;
+            this.IMI = IMI;
+        }
+
+        public Car(Int16 CarId, string CarType, string Brand, string Model, double FuelConsumption, double EnergyConsumption, double Weight, Int16 Capacity, Int64 IMI, string Username) {
             this.CarId = CarId;
             this.CarType = CarType;
             this.Brand = Brand;
@@ -28,14 +33,16 @@ namespace CarDataProject {
         }
 
         public Car(DataRow row) {
-            this.CarId = row.Field<int>("carid");
-            this.CarType = row.Field<string>("cartype");
-            this.Brand = row.Field<string>("brand");
-            this.Model = row.Field<string>("model");
-            this.FuelConsumption = row.Field<double>("fuelconsumption");
-            this.EnergyConsumption = row.Field<double>("energyconsumption");
-            this.Weight = row.Field<double>("weight");
-            this.Capacity = row.Field<Int16>("capacity");
+            this.CarId = row.Field<Int16>("carid");
+            if (row.Table.Columns.Contains("cartype")) {
+                this.CarType = row.Field<string>("cartype");
+                this.Brand = row.Field<string>("brand");
+                this.Model = row.Field<string>("model");
+                this.FuelConsumption = row.Field<double>("fuelconsumption");
+                this.EnergyConsumption = row.Field<double>("energyconsumption");
+                this.Weight = row.Field<double>("weight");
+                this.Capacity = row.Field<Int16>("capacity");
+            }
             this.IMI = row.Field<Int64>("imi");
             this.Username = row.Field<string>("username");
         }
