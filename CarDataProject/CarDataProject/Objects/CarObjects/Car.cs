20b861ffedb6b5e4b1a08c4cfa43f6830b0a5f84
Johan Leth Gregersen <johangregersen@live.dk>
2016-04-22 14:59:20 +0200

    Added some instantieringsmetoder ud fra datarows til competition og car.

--- CarDataProject/CarDataProject/Objects/CarObjects/Car.cs
+++ CarDataProject/CarDataProject/Objects/CarObjects/Car.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace CarDataProject {
     public class Car {
@@ -10,8 +11,10 @@ namespace CarDataProject {
         public double EnergyConsumption { get; }
         public double Weight { get; }
         public Int16 Capacity { get; }
+        public Int64 IMI { get; set; }
+        public string Username { get; set; }
 
-        public Car(int CarId, string CarType, string Brand, string Model, double FuelConsumption, double EnergyConsumption, double Weight, Int16 Capacity) {
+        public Car(int CarId, string CarType, string Brand, string Model, double FuelConsumption, double EnergyConsumption, double Weight, Int16 Capacity, Int64 IMI, string Username) {
             this.CarId = CarId;
             this.CarType = CarType;
             this.Brand = Brand;
@@ -20,6 +23,21 @@ namespace CarDataProject {
             this.EnergyConsumption = EnergyConsumption;
             this.Weight = Weight;
             this.Capacity = Capacity;
+            this.IMI = IMI;
+            this.Username = Username;
+        }
+
+        public Car(DataRow row) {
+            this.CarId = row.Field<int>("carid");
+            this.CarType = row.Field<string>("cartype");
+            this.Brand = row.Field<string>("brand");
+            this.Model = row.Field<string>("model");
+            this.FuelConsumption = row.Field<double>("fuelconsumption");
+            this.EnergyConsumption = row.Field<double>("energyconsumption");
+            this.Weight = row.Field<double>("weight");
+            this.Capacity = row.Field<Int16>("capacity");
+            this.IMI = row.Field<Int64>("imi");
+            this.Username = row.Field<string>("username");
         }
     }
 }
--- CarDataProject/CarDataProject/Objects/CompetitionObjects/Competition.cs
+++ CarDataProject/CarDataProject/Objects/CompetitionObjects/Competition.cs
@@ -3,8 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace CarDataProject {
     public class Competition {
+        public Int16 CompetitionId { get; set; }
+        public int StartDateId { get; set; }
+        public int StartTimeId { get; set; }
+        public int StopDateId { get; set; }
+        public int StopTimeId { get; set; }
+        public string CompetitionDescription { get; set; }
+
+        public Competition(Int16 CompetitionId, int StartDateId, int StartTimeId, int StopDateId, int StopTimeId, string CompetitionDescription) {
+            this.CompetitionId = CompetitionId;
+            this.StartDateId = StartDateId;
+            this.StartTimeId = StartTimeId;
+            this.StopDateId = StopDateId;
+            this.StopTimeId = StopTimeId;
+            this.CompetitionDescription = CompetitionDescription;
+        }
+
+        public Competition(DataRow row) {
+            this.CompetitionId = row.Field<Int16>("competitionid");
+            this.StartDateId = row.Field<int>("startdateid");
+            this.StartTimeId = row.Field<int>("starttimeid");
+            this.StopDateId = row.Field<int>("stopdateid");
+            this.StopTimeId = row.Field<int>("stoptimeid");
+            this.CompetitionDescription = row.Field<string>("competitiondescription");
+        }
     }
 }
