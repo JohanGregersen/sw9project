fe1ac3d27fe7c60e9f3b0d10e899581094a23741
Johan Leth Gregersen <johangregersen@live.dk>
2016-05-20 11:52:49 +0200

    I made a god damn correlation matrix!

--- CarDataProject/CarDataProject/Comparison Testing/MetricCorrelation.cs
+++ CarDataProject/CarDataProject/Comparison Testing/MetricCorrelation.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace CarDataProject {
     class MetricCorrelation {
         public static Double Correlation(double[] FirstArray, double[] SecondArray) {
+            
             Double sumX = 0;
             Double sumX2 = 0;
             Double sumY = 0;
@@ -31,6 +32,58 @@ namespace CarDataProject {
             Double covariance = (sumXY / n - sumX * sumY / n / n);
 
             return covariance / stdX / stdY;
+        }
+
+        public static Double[,] getCorrelationMatrix(List<Trip> trips) {
+
+            Double[,] CorrelationMatrix = new Double[6,6];
+            
+            List<Double[]> pearsonvalues = new List<double[]>();
+
+            Double[] Roadtypes = new Double[trips.Count()];
+            Double[] CriticalTime = new Double[trips.Count()];
+            Double[] Speeding = new Double[trips.Count()];
+            Double[] Accelerating = new Double[trips.Count()];
+            Double[] Braking = new Double[trips.Count()];
+            Double[] Jerking = new Double[trips.Count()];
+           
+            for(int i = 1; i < trips.Count; i++) {
+                Roadtypes[i] = trips.ElementAt(i).RoadTypeScore;
+                CriticalTime[i] = trips.ElementAt(i).CriticalTimeScore;
+                Speeding[i] = trips.ElementAt(i).SpeedingScore;
+                Accelerating[i] = trips.ElementAt(i).AccelerationScore;
+                Braking[i] = trips.ElementAt(i).BrakeScore;
+                Jerking[i] = trips.ElementAt(i).JerkScore;
+            }
+
+            pearsonvalues.Add(Roadtypes);
+            pearsonvalues.Add(CriticalTime);
+            pearsonvalues.Add(Speeding);
+            pearsonvalues.Add(Accelerating);
+            pearsonvalues.Add(Braking);
+            pearsonvalues.Add(Jerking);
+
+            for(int i = 0; i < pearsonvalues.Count; i++) {
+                for(int j = 0; j < pearsonvalues.Count; j++) {
+                    CorrelationMatrix[i,j] = Correlation(pearsonvalues[i], pearsonvalues[j]);
+                }
+            }
+            
+            return CorrelationMatrix;
+        }
+
+        public static void printMatrix(Double[,] matrix) {
+
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++) {
+                for (int j = 0; j < colLength; j++) {
+                    Console.Write(string.Format("{0} ", matrix[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ReadLine();
         }
     }
 }
